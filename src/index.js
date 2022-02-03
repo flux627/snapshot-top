2d009887e5aaecc7254273dacbd1626fabd2cdc2
Julien Heller <flux627@users.noreply.github.com>
2022-02-03 16:59:24 -0500

    Add ability to see results before voting closes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import fetch from 'node-fetch'
-import {getChoices, getVotes} from "./queries.js";
-import {findMaxIndices} from "./findMaxIndices.js";
+import {getChoices, getVotes, getVotingPowers} from "./queries.js"
+import {findMaxIndices} from "./findMaxIndices.js"
 
-const proposalId = process.argv[2] || '0xa0220651df3d1e501f211f02bd81f3e00e2d182086a8b4cec5eee5595eb6bb5e';
+const proposalId = process.argv[2] || '0xbef622be197b4b238d034e41b95c7c3906a7882d9c807c6e8c4841859bcf0e6d'
 const topCount = process.argv[3] || 7;
 
 (async () => {
   const choices = await getChoices(proposalId)
   const votes = await getVotes(proposalId)
+  const votingPowers = await getVotingPowers(proposalId, votes) // used before votes finalized
   const totals = Array(choices.length).fill(0)
   for (const vote of votes) {
     for (let i = 0; i < vote.choice.length || i < topCount; i++) {
+      if (vote.vp === 0) {
+        vote.vp = votingPowers[vote.voter]
+      }
       totals[vote.choice[i] - 1] += vote.vp
     }
   }
--- src/queries.js
+++ src/queries.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+import fetch from "node-fetch"
 
 export async function getVotes(proposalId) {
   const response = await fetch('https://hub.snapshot.org/graphql', {
@@ -12,7 +12,7 @@ export async function getVotes(proposalId) {
   return JSON.parse(await response.text()).data.votes
 }
 
-export async function getChoices(proposalId) {
+export async function getProposal(proposalId) {
   const response = await fetch('https://hub.snapshot.org/graphql', {
     method: 'POST',
     headers: {
@@ -21,5 +21,32 @@ export async function getChoices(proposalId) {
     body: `{"operationName":"Proposal","variables":{"id":"${proposalId}"},"query":"query Proposal($id: String!) {\\n  proposal(id: $id) {\\n    id\\n    ipfs\\n    title\\n    body\\n    choices\\n    start\\n    end\\n    snapshot\\n    state\\n    author\\n    created\\n    plugins\\n    network\\n    type\\n    strategies {\\n      name\\n      params\\n    }\\n    space {\\n      id\\n      name\\n    }\\n    scores_state\\n    scores\\n    scores_by_strategy\\n    scores_total\\n    votes\\n  }\\n}"}`,
   })
 
-  return JSON.parse(await response.text()).data.proposal.choices
+  return JSON.parse(await response.text()).data.proposal
+}
+
+export async function getChoices(proposalId) {
+  return (await getProposal(proposalId)).choices
+}
+
+export async function getVotingPowers(proposalId) {
+  const proposal = await getProposal(proposalId)
+  const votes = await getVotes(proposalId)
+  const voteAddresses = votes.map((vote) => vote.voter)
+  const response = await fetch('https://score.snapshot.org/api/scores', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      params: {
+        space: proposal.space.id,
+        network: proposal.network,
+        snapshot: parseInt(proposal.snapshot),
+        strategies: proposal.strategies,
+        addresses: voteAddresses,
+      }
+    })
+  })
+
+  return (await response.json()).result.scores[0]
 }
